d1731ff3b127dcd26b0ede3c5a4d9557ae414682
Amit Motgi <amotgi@walmartlabs.com>
2017-05-20 20:31:25 -0700

    updating

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
-var path = require('path');
-var express = require('express')
-var app = express()
-var port = process.env.PORT || "3000";
-var fs = require('fs');
-var net = require('net');
+const path = require('path');
+const express = require('express')
+const app = express()
+const port = process.env.PORT || "3000";
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(process.env.PORT || 3000, function () {
+app.listen(process.env.PORT || 3000, () => {
   console.log('Example app listening on port 3000!')
 });
